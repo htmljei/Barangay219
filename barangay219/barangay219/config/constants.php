40550808bb075d54636b9a413e3a7dcad33a11de
htmljei <pagal.jericho@ue.edu.ph>
2026-01-26 23:04:04 +0800

    Update constants.php

--- barangay219/barangay219/config/constants.php
+++ barangay219/barangay219/config/constants.php
@@ -11,7 +11,7 @@ if (!defined('ACCESS_ALLOWED')) {
 
 // Database Configuration
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'ebarangay_db');
+define('DB_NAME', 'barangay219_db');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_CHARSET', 'utf8mb4');
